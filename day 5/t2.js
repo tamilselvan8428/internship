df226734f7c60c8753d8ff8ecf28186debfeb6de
tamilselvan8428 <tamilselvan24650@gmail.com>
2025-01-10 13:01:04 +0530

    first

--- day 5/t2.js
+++ day 5/t2.js
@@ -1,5 +1,4 @@
-let a=-
-10;
+let a=-10;
 if(a>=0 && a%2==0){
     console.log("positive and even number");
 }
@@ -11,4 +10,20 @@ else if(a<0 && a%2==0){
 }
 else{
     console.log("negative and odd number");
+}
+
+//grade
+
+let b=prompt("enter the mark:");
+if(b>=90){
+    console.log("grade a");
+}
+else if(b>=70 && b<90){
+    console.log("grade b");
+}
+else if(b>=50 && b<70){
+    console.log("grade c");
+}
+else{
+    console.log("fail");
 }
